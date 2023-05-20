2cebc795ef95a243925c0cf62af4968f7d36a701
vasko <vasko@DESKTOP-IF404CF>
2023-05-20 18:36:25 +0300

    Added tests

---
+++ WpfTestTask/Migrations/20230520151438_AddProcedureAndIndex.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfTestTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcedureAndIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+CREATE PROCEDURE getCustomers
+@name NVARCHAR(50) =  NULL,
+@companyName NVARCHAR(50) =  NULL,
+@phone NVARCHAR(50) =  NULL,
+@email NVARCHAR(50) =  NULL
+as
+BEGIN
+SELECT c.Id, c.Name, c.CompanyName, c.Phone, c.Email FROM Customers c
+WHERE (@name IS NULL OR c.Name LIKE '%'+@name+'%') AND
+(@companyName IS NULL OR c.CompanyName LIKE '%'+@companyName+'%') AND
+(@phone IS NULL OR c.Phone LIKE '%'+@phone+'%') AND
+(@email IS NULL OR c.Email LIKE '%'+@email+'%');
+END
+");
+
+            migrationBuilder.Sql("CREATE INDEX name_index ON Customers (Name);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX name_index ON Customers");
+            migrationBuilder.Sql("DROP PROCEDURE getCustomers");
+        }
+    }
+}
---
+++ WpfTestTaskTests/DbRepositoryTests/DbRepositoryTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.DbRepository;
+using WpfApp2.Model;
+
+namespace WpfTestTaskTests.DbRepositoryTests
+{
+    [TestFixture]
+    internal class DbRepositoryTests
+    {
+        private DbRepository _dbRepository;
+        private Customer _customerToDelete;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dbRepository = DbRepositorySingelton.GetInstance().GetDbRepository();
+            _customerToDelete = new Customer();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_customerToDelete.Id != 0)
+            {
+                _dbRepository.DeleteData(_customerToDelete);
+            }
+        }
+
+        [Test]
+        public void AddCustomerTest()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                Name = "TestName",
+                CompanyName = "TestCompanyName",
+                Phone = "TestPhone",
+                Email = "TestEmail"
+            };
+
+            // Act
+            _dbRepository.AddData(customer);
+            List<Customer> customerList = _dbRepository.FetchData();
+            var addedCustomer = customerList.FirstOrDefault(t => t.Name.Equals(customer.Name));
+            _customerToDelete = addedCustomer;
+
+            // Assert
+            Assert.AreEqual(customer.Name, addedCustomer.Name);
+            Assert.AreEqual(customer.CompanyName, addedCustomer.CompanyName);
+            Assert.AreEqual(customer.Phone, addedCustomer.Phone);
+            Assert.AreEqual(customer.Email, addedCustomer.Email);
+        }
+
+        [Test]
+        public void UpdateCustomerTest()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                Name = "TestName",
+                CompanyName = "TestCompanyName",
+                Phone = "TestPhone",
+                Email = "TestEmail"
+            };
+
+            // Act
+            _dbRepository.AddData(customer);
+            List<Customer> customerList = _dbRepository.FetchData();
+            var addedCustomer = customerList.FirstOrDefault(t => t.Name.Equals(customer.Name));
+            addedCustomer.Name = "TestName1";
+            addedCustomer.CompanyName = "TestCompanyName1";
+            addedCustomer.Phone = "TestPhone1";
+            addedCustomer.Email = "TestEmail1";
+            _dbRepository.UpdateDate(addedCustomer);
+            var updatedCustomer = customerList.FirstOrDefault(t => t.Name.Equals(addedCustomer.Name));
+            _customerToDelete = updatedCustomer;
+
+            // Assert
+            Assert.AreEqual(addedCustomer.Name, updatedCustomer.Name);
+            Assert.AreEqual(addedCustomer.CompanyName, updatedCustomer.CompanyName);
+            Assert.AreEqual(addedCustomer.Phone, updatedCustomer.Phone);
+            Assert.AreEqual(addedCustomer.Email, updatedCustomer.Email);
+        }
+
+        [Test]
+        public void DeleteCustomerTest()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                Name = "TestName",
+                CompanyName = "TestCompanyName",
+                Phone = "TestPhone",
+                Email = "TestEmail"
+            };
+
+            // Act
+            _dbRepository.AddData(customer);
+            List<Customer> customerList = _dbRepository.FetchData();
+            var addedCustomer = customerList.FirstOrDefault(t => t.Name.Equals(customer.Name));
+            _dbRepository.DeleteData(addedCustomer);
+            customerList = _dbRepository.FetchData();
+            var deletedCustomer = customerList.FirstOrDefault(t => t.Name.Equals(customer.Name));
+
+            // Assert
+            Assert.IsNull(deletedCustomer);
+        }
+    }
+}
